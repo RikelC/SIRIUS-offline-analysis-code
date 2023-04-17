ba16512ae26f238be2ec2d5c4a935f717f3b64e5
Rikel Chakma <34173843+RikelC@users.noreply.github.com>
2023-04-17 19:54:58 +0200

    Add files via upload

--- build/doCmake.sh
+++ build/doCmake.sh
@@ -2,6 +2,6 @@
 rm -rf CMakeFiles
 rm -f CMakeCache.txt cmake_install.cmake Makefile siriusTest
 sleep 1
-cmake ../program
+cmake -DNO_MFMNXML=YES -DCMAKE_INSTALL_PREFIX=../ ../program
 sleep 1
 make
